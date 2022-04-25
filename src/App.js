4dbf2f801073f8c1451092cb3f1cd36d23870d21
mohitraj610 <mohitraj1999@gmail.com>
2022-04-25 16:46:20 +0530

    v3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {Button, Modal} from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
@@ -28,13 +28,20 @@ function App() {
    })
   
   }
-  
+
+  let width = 465;
+  let height = 280;
+  if(window.innerHeight<=500)
+  {
+    width=300;
+    height=180;
+  }
 
   return (
-    <div>
-      <Modal show={show} onHide={closeModal}>
-      <Modal.Header><div className='app'>Checkout what you can do in our FREE trial class!</div><img className='imgbutton' src="https://www.wiingy.com/wp-content/themes/wingynew/details_assets/images/login-close.svg" onClick={closeModal} /></Modal.Header>
-      <Modal.Body><video width="465" height="280" muted="false" autoplay="true" loop="true"><source src="https://www.wiingy.com/wp-content/uploads/2022/04/Wiingy-youtube-14sec-1.webm" type="video/mp4" /><source src="https://www.wiingy.com/wp-content/uploads/2022/04/Wiingy-youtube-14sec-1.webm" type="video/ogg" />Your browser does not support the video tag. </video>
+    <div >
+      <Modal contentClassName='total' show={show} onHide={closeModal}>
+      <Modal.Header className='header'><div className='app'>Checkout what you can do in our FREE trial class!</div><img className='imgbutton' src="https://www.wiingy.com/wp-content/themes/wingynew/details_assets/images/login-close.svg" alt= "" onClick={closeModal} /></Modal.Header>
+      <Modal.Body><video width={width} height={height} muted="false" autoplay="true" loop="true"><source src="https://www.wiingy.com/wp-content/uploads/2022/04/Wiingy-youtube-14sec-1.webm" type="video/mp4" /><source src="https://www.wiingy.com/wp-content/uploads/2022/04/Wiingy-youtube-14sec-1.webm" type="video/ogg" />Your browser does not support the video tag. </video>
       <div className='animation'><Button>Book A Free Trial</Button></div></Modal.Body>
       </Modal>
     </div>
